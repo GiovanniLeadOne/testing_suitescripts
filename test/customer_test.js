63fbe8f9682553edcab7dc2722dc49bd1fbfdb06
GiovanniLeadOne <Giovanni.Arrazola@leadonemx.com>
2019-10-09 18:52:22 -0500

    example github whit json

--- test/customer_test.js
+++ test/customer_test.js
@@ -1,33 +1,39 @@
 'use strict';
-
 var should = require('should');
-var expect = require('chai').expect;
 var nsmockup = require('nsmockup');
 
-var _dirnameCustomer = "app/customer.json"
+var base = "app"
 /**
  * Test Suitelet
  */
  var done;
- describe('Unit Test Netsuite -Record', function()
- {
-   before(done = function(){
-      // map record types
-       let records = {
-              'customer': _dirnameCustomer 
+ describe('<Unit Test - Netsuite Record API>', function () {
+  this.timeout(5000);
+
+  before(done => {
+      let metadata = [
+              base + '/meta/customrecord_codeg.json',
+              base + '/meta/customrecord_codeg_ids.json'
+          ],
+          records = {
+              'customrecord_codeg': base + '/data/customrecord_codeg.json',
+              'customrecord_codeg_ids': base + '/data/customrecord_codeg_ids.json'
           };
+      nsmockup.init({records, metadata}, done);
+  });
+  describe('SuiteScript API - nlapiLoadRecord:', () => {
+      let recType = 'customrecord_codeg';
 
-      // start database simulation
-      nsmockup.init({records, server: true}, done);
-    });
+      it('load by id', done => {
+          let code = nlapiLoadRecord(recType, 1);
+          should(code).have.instanceOf(nlobjRecord);
+          should(code.getAllFields()).have.length(2);
 
-    describe("Record", function() 
-    {
-        let recordcustomer = 'customer'
-        it("Load by id", function()
-        {
-           expect(1+1).to.equal(2) 
-        });
-        return done();
+          ['custrecord_type_id', 'custrecord_code_id'].forEach(f => {
+              should(code.getFieldValue(f)).be.ok();
+          });
+
+          return done();
+      }); 
     });
  });
