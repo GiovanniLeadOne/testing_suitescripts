bf8452292d5f335633dcd3a933063ac1ee999595
Giovanni Arrazola <Giovanni.Arrazola@leadonemx.com>
2019-10-10 13:28:42 -0500

    Start file test blank

--- test/customer_test.js
+++ test/customer_test.js
@@ -7,31 +7,8 @@ var base = "app/customer.json"
 /**
  * Test Suites
  */
-describe('<Unit Test - Netsuite Context API>', function () {
-    describe('SuiteScript API - nlapiGetUser:', () => {        
-
-        it('get-user current "customer whit id 22"', done => {
-            let opts = {
-                metadata: [':customer'],
-                records: {
-                    'customer': base 
-                },
-                current: {
-                    user: {
-                        id: 22,
-                        type: 'customer'
-                    }
-                }
-            };
-            nsmockup.init(opts, (err) => {
-                if (err) return done(err);
-
-
-                let id = nlapiGetUser();
-                should(id).be.equal('22');
-
-                nsmockup.destroy(done);
-            });
-        });        
+describe('<Unit Test - Netsuite Customer JSON>', function () {
+    describe('SuiteScript Practice - nlapiLoadRecord:', () => {                        
+        
     });
 });
