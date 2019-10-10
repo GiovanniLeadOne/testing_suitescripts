ce98d0811d5619bb6182c207b7e4c7f8cad9b899
Giovanni Arrazola <Giovanni.Arrazola@leadonemx.com>
2019-10-10 15:58:57 -0500

    Read JSON not current user

--- test/customer_test.js
+++ test/customer_test.js
@@ -1,14 +1,40 @@
 'use strict';
 
 var should = require('should'),
-    nsmockup = require('nsmockup');
+    nsmockup = require('nsmockup'),
+    fs = require('fs'),
+    path = require('path');
 
-var base = "app/customer.json"
+var base = "app/customer.json";
 /**
  * Test Suites
  */
+var customerpath = path.resolve(base),
+                customerjson = JSON.parse(fs.readFileSync(customerpath));
+
 describe('<Unit Test - Netsuite Customer JSON>', function () {
-    describe('SuiteScript Practice - nlapiLoadRecord:', () => {                        
-        
+    describe('SuiteScript Practice - nlapiLoadRecord:', () => {              
+
+        it('get-user current "customer"', done => {
+            let opts = {
+                metadata: [':customer'],
+                records: {
+                    'customer': base
+                },
+                current: {
+                    user: {id: 22, type:'customer'}
+                }                
+            };
+            nsmockup.init(opts, (err) => {
+                if (err) return done(err);
+
+                let id = nlapiGetUser();
+                should(id).be.equal('22');
+                //show terminal message
+                nlapiLogExecution("DEBUG", customerjson)
+                nsmockup.destroy(done);
+            });
+        });
+
     });
 });
