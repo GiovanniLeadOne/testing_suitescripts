6978ebf514c18d8e6e4f354ff87a01ef6d54548f
Giovanni Arrazola <Giovanni.Arrazola@leadonemx.com>
2019-10-11 16:30:56 -0500

    create file event user and test event user

---
+++ app/user_event.js
@@ -0,0 +1,14 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType UserEventScript
+ */
+define(['N/log', 'N/ui/dialog'], function() {
+
+    function beforeLoad(context) {
+        
+    }
+
+    return {
+        beforeLoad: beforeLoad        
+    }
+});
